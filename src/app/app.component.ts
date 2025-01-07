94bb17a5db73f2795df43b94b6966e90789c4b44
Sigmund Frost <sigmafi.tech@gmail.com>
2025-01-07 21:26:34 +0100

    Testing footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {
+  RouterLink,
+  RouterOutlet,
+  NavigationEnd,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'generic-blog';
+  showGoBackButton = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showGoBackButton = event.url !== '/';
+      }
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,11 +1,11 @@
 import { ApiService } from '../../api/api.service';
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
+  imports: [AsyncPipe],
   templateUrl: './posts.component.html',
 })
 export class PostsComponent {
