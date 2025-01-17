8f2fce5519c98c8fa443bb79b72e7e609cb94661
Sigmund Frost <sigmafi.tech@gmail.com>
2025-01-17 20:50:30 +0100

    Added tinymce

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostComponent } from './components/post/post.component';
+import { TinymceComponent } from './components/tinymce/tinymce.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'post',
     component: PostComponent,
+  },
+  {
+    path: 'tinymce',
+    component: TinymceComponent,
   },
 ];
